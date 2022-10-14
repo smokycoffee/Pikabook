ad60ec3ed4fd3ae9f4ad653e5eda8ce02d6ca973
Tsenguun <Tsenguun.batbold12@gmail.com>
2022-10-14 19:33:44 +0200

    initial commit

---
+++ Poke-companion/Poke_companionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poke_companionApp.swift
+//  Poke-companion
+//
+//  Created by Tsenguun on 14/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Poke_companionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
