939d5420339f1d884ac87488f8bffdb7bb974385
Tsenguun <Tsenguun.batbold12@gmail.com>
2022-11-16 00:19:10 +0100

    PokedexViewModel fixed, to add pagination

--- Poke-companion/ViewModels/PokedexViewModel.swift
+++ Poke-companion/ViewModels/PokedexViewModel.swift
@@ -21,56 +21,13 @@ class PokedexViewModel: ObservableObject {
     
     var cancellables: Set<AnyCancellable> = []
     
-    func fetchPokemons() {
-        let url = URL(string: "https://pokeapi.co/api/v2/pokemon?limit=151")!
-        
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        
-        URLSession.shared.dataTaskPublisher(for: url)
-            .map { $0.data }
-            .decode(type: Pokedex.self, decoder: decoder)
-            .receive(on: RunLoop.main)
-            .sink { completion in
-            } receiveValue: { [unowned self] pokedex in
-                for i in pokedex.results {
-                    self.fetchPokemonInfo(for: i.url)
-                    loading = false
-                }
-                //                print(pokemonDataToView)
-                self.pokemonURL = pokedex.results
-//                print(pokemonURL)
-            }
-            .store(in: &cancellables)
-    }
-    
-    func fetchPokemonInfo(for pokemonUrl: String) {
-        
-        let url = URL(string: pokemonUrl)!
-        
-        let decoder = JSONDecoder()
-        
-        URLSession.shared.dataTaskPublisher(for: url)
-            .map { $0.data }
-            .decode(type: Pokemon.self, decoder: decoder)
-            .receive(on: RunLoop.main)
-            .sink { completion in
-                //                print(completion)
-            } receiveValue: { [unowned self] pokemon in
-                if loading == false {
-                    pokemonDataToView.append(pokemon)
-                }
-            }
-            .store(in: &cancellables)
-    }
-    
     /// Fetch a pokemon from URL
     /// - Parameter urlString: URL of the pokemon
     /// - Returns: A publisher with output PokemonDetailResponse
     func getPokemon (_ urlString: String) -> AnyPublisher<Pokemon, Error> {
         let url = URL(string: urlString)!
         return URLSession.shared.dataTaskPublisher(for: url)
-                .map(\.data)
+                .tryMap(\.data)
                 .decode(type: Pokemon.self, decoder: JSONDecoder())
                 .receive(on: RunLoop.main)
                 .eraseToAnyPublisher()
@@ -83,7 +40,7 @@ class PokedexViewModel: ObservableObject {
     func getPokemonList (urlString: String) ->  AnyPublisher<Pokedex, Error> {
         let url = URL(string: urlString)!
         return URLSession.shared.dataTaskPublisher(for: url)
-            .map(\.data)
+            .tryMap(\.data)
             .decode(type: Pokedex.self, decoder: JSONDecoder())
             .receive(on: RunLoop.main)
             .eraseToAnyPublisher()
@@ -93,8 +50,8 @@ class PokedexViewModel: ObservableObject {
     /// Fetch a list of pokemon with details
     /// - Returns: A publisher with output an array of PokemonDetailResponse
     func getPokemonListWithDetails () -> AnyPublisher<[Pokemon], Error> {
-        return getPokemonList(urlString: "https://pokeapi.co/api/v2/pokemon?offset=0&limit=250")
-            .map(\.results)
+        return getPokemonList(urlString: "https://pokeapi.co/api/v2/pokemon?offset=0&limit=151")
+            .tryMap(\.results)
             .flatMap {
                 Publishers
                     .MergeMany($0.map { self.getPokemon($0.url) })
@@ -110,7 +67,7 @@ class PokedexViewModel: ObservableObject {
             print("done")
         } receiveValue: { (pokemons) in
             print(pokemons.map({ "- \($0.name):" }).joined(separator: "\n"))
-            self.pokemonListArray = pokemons
+            self.pokemonListArray.append(contentsOf: pokemons)
         }
         .store(in: &cancellables)
     }
